7d4f2b69fcc8d23f648cce2ff8a9bbd5be64e1c6
frhnn772 <farhan772ajaa@gmail.com>
2022-07-09 15:42:00 +0700

    tambah smooth scrolling

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,20 +1,3 @@
-// Navbar Fixed dan to top
-window.onscroll = function() {
-    const header = document.querySelector('header');
-    const fixedNav = header.offsetTop;
-    // const toTop = document.querySelector('#to-top');
-
-    if(window.pageYOffset > fixedNav){
-        header.classList.add('navbar-fixed');
-        // toTop.classList.remove('hidden');
-        // toTop.classList.add('flex');
-    }else{
-        header.classList.remove('navbar-fixed')
-        // toTop.classList.remove('flex');
-        // toTop.classList.add('hidden');
-    }
-}
-
 // Hamburger
 const hamburger = document.querySelector('#hamburger');
 const navMenu = document.querySelector('#nav-menu');
@@ -22,14 +5,6 @@ const navMenu = document.querySelector('#nav-menu');
 hamburger.addEventListener('click', function(){
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
-});
-
-// klik diluar hamburger
-window.addEventListener('click', function(e){
-    if(e.target != hamburger && e.target != navMenu) {
-        hamburger.classList.remove('hamburger-active');
-        navMenu.classList.add('hidden');
-    }
 });
 
 // typing
@@ -40,24 +15,3 @@ typeSpeed:175,
 backSpeed:85,
 loop:true,
 });
-
-// dark-mode toggle
-const darkToggle = document.querySelector('#dark-toggle');
-const html = document.querySelector('html');
-
-darkToggle.addEventListener('click', function() {
-    if (darkToggle.checked) {
-        html.classList.add('dark');
-        localStorage.theme = 'dark';
-    } else {
-        html.classList.remove('dark');
-        localStorage.theme = 'light';
-    }
-});
-
-// pindahkan posisi toggle sesuai mode
-if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    darkToggle.checked = true;
-} else {
-    darkToggle.checked = false;
-  }
